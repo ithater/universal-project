7deb21e14fe562ad7d1b6119c070b23f72287215
ithater <ithater.iwnl@gmail.com>
2020-10-13 16:45:12 +0300

    create: recommended-tabs

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import 'normalize.css';
 import './sass/index.sass';
 
 import menuMain from './modules/menuMain';
+import recomendationTabs from './modules/recomendationTabs';
 
 
 menuMain();
+recomendationTabs();
