65ada460fdae8f3a22b0da2960a7039d9b86d554
ithater <ithater.iwnl@gmail.com>
2020-10-20 15:50:44 +0300

    .

--- src/modules/menuMain.js
+++ src/modules/menuMain.js
@@ -1,11 +1,9 @@
 const menuMain = () => {
 	const menuBtn = document.querySelector('.header .burger');
-	const headerNavbar = document.querySelector('.header-navbar');
-	const headerNavbarSearch = headerNavbar.querySelector('.header-navbar__search');
+  const headerNavbar = document.querySelector('.header-navbar');
 
 	const open = () => {
 		headerNavbar.classList.toggle('header-navbar--mobile-visible');
-		headerNavbarSearch.classList.toggle('header-navbar__search--mobile-visible');
 	};
 
 	menuBtn.addEventListener('click', open);
